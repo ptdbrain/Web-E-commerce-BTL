1d4203705c604667ee3d69f3b87800ea28fb94ec
PTDBrain <phandat20052009@gmail.com>
2026-05-20 23:01:59 +0700

    chore: remove duplicated root project files

--- frontend/src/hooks/useCart.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { CartContext } from "../contexts/CartContext";
-
-export function useCart() {
-  return useContext(CartContext);
-}
--- frontend/src/pages/admin/AdminProductsPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AdminProducts from '../../components/admin/AdminProducts.jsx';
-
-const AdminProductsPage = () => {
-  return <AdminProducts />;
-};
-
-export default AdminProductsPage;
